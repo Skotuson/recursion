9119237e62e4dcc46dd50c7cd38f63e1e25346c2
Skotuson <58478268+Skotuson@users.noreply.github.com>
2022-01-28 22:48:05 +0100

    general: edit test script

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ clear
 g++ -Wall -pedantic "$1" -fsanitize=address -g -o a
 if [ "$#" -eq 3 ]; then {
     if [ "$2" = "to" ]; 
-        then a > "$3";
+        then ./a > "$3";
     elif [ "$2" = "from" ]; 
-        then a < "$2";
+        then ./a < "$2";
     fi
 } else ./a
 fi
