b7f0ec383dabbd668f712d428185bad5df226282
Skotuson <58478268+Skotuson@users.noreply.github.com>
2022-02-02 19:03:29 +0100

    bin: initial commit, boilerplate

---
+++ bin/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LEN 3
+
+void printBinary ( int * a, int len ) {
+    for ( int i = 0; i < len; i++ )
+        printf( "%d", a[i] );
+    printf( "\n" );
+}
+
+void binary ( int * a, int k, int pos ) {
+
+}
+
+int main ( void ) {
+    int * a = ( int* ) calloc ( LEN, sizeof ( *a ) );
+    binary ( a, LEN, 0 );
+    free ( a );
+    return 0;
+}
