786c1c7d33b74f9f56851b857dad90156da83d87
Skotuson <58478268+Skotuson@users.noreply.github.com>
2022-02-06 20:34:57 +0100

    bsum: final impl

--- bsum/bsum.c
+++ bsum/bsum.c
@@ -9,14 +9,45 @@ void print_bin ( int * bin, int len ) {
     printf ( "\n" );
 }
 
-void bsum ( int * bin, int k, int pos, int n ) {
-    
+int bsum ( int * bin, int len, int pos, 
+            int next, int n, int sum ) {
+
+    if ( pos == len ) {
+        print_bin ( bin, len );
+        return 1;
+    }
+
+    int cnt = 0;
+    bin[pos] = next;
+
+    if ( pos < ( len / 2 ) - 1 ) {
+        cnt += bsum ( bin, len, pos + 1, 0, n, sum );
+        cnt += bsum ( bin, len, pos + 1, 1, n, sum + 1 );
+    } else {
+        //If either one of these conditions is true, the rest will be filled with 0's or 1's
+        if ( ! sum )
+            cnt += bsum ( bin, len, pos + 1, 0, n, sum );
+        else if ( n == sum )
+             cnt +=bsum ( bin, len, pos + 1, 1, n, sum );
+        //------------------------------------------------------------------------------------
+        else {
+            cnt += bsum ( bin, len, pos + 1, 0, n - 1, sum );
+            cnt += bsum ( bin, len, pos + 1, 1, n - 1, sum - 1 );
+        } 
+    }
+
+    return cnt;
 }
 
 int main ( void ) {
 
     int * bin = ( int* ) calloc ( N * 2, sizeof ( *bin ) );
-    bsum ( bin, N * 2, 0, N );
+    int cnt = 0;
+   
+    cnt += bsum ( bin, N * 2, 0, 0, N, 0 );
+    cnt += bsum ( bin, N * 2, 0, 1, N, 1 );
+    printf( " = %d\n", cnt );
+   
     free ( bin );
     return 0;
 
