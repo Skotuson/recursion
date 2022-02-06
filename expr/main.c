5e8a77940551d5a7afecdaaf39eae7aa1d86e830
Skotuson <58478268+Skotuson@users.noreply.github.com>
2022-02-06 20:40:35 +0100

    expr: initial commit

---
+++ expr/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void expr ( const char * in, int t ) {
+    //TODO
+}
+
+int main ( void ) {
+
+    const char * input = "125";
+    int target = 7;
+
+    expr ( input, target );
+
+    return 0;
+}
